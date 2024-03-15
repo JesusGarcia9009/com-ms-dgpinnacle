7f84d88bde5ea0da5cd091b46aaedad2b5526c29
Jesus Garcia <jesus.garcia@open2000.cl>
2024-03-15 17:48:48 -0300

    (feat) url security config

--- src/main/java/com/ms/dgpinnacle/utils/ConstantUtil.java
+++ src/main/java/com/ms/dgpinnacle/utils/ConstantUtil.java
@@ -25,7 +25,7 @@ public class ConstantUtil {
 	 * Security
 	 */
 	public static final String[] PATH_ARRAY = new String[] { "/**/v2/api-docs", "/configuration/ui", "/swagger-resources/**",
-			"/api/gcom/operacional/login/**", "/configuration/security", "/**/swagger-ui/**", "/swagger-ui.html",
+			"/api/dgpinnacle/login/**", "/configuration/security", "/**/swagger-ui/**", "/swagger-ui.html",
 			"/webjars/**" };
 	public static final String[] PATH_RESOURCES_ARRAY = new String[] { "/", "/favicon.ico", "/**/*.png", "/**/*.gif", "/**/*.svg", "/**/*.jpg",
 			"/**/*.html", "/**/*.css", "/**/*.js" };
