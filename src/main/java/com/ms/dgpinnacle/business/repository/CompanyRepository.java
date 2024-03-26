525d0ffc5beeb6ddb65fbf6baed29955ca5566a0
Jesus Garcia <jesus.garcia@open2000.cl>
2024-03-26 12:45:42 -0300

    (feat) se eliminan archivos inmnecesarios

--- src/main/java/com/ms/dgpinnacle/business/repository/CompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ms.dgpinnacle.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.ms.dgpinnacle.entity.Company;
-
-public interface CompanyRepository extends CrudRepository<Company, Long> {
-
-}
